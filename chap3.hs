37803ca178201217f348e20c6b959a66e26831c3
cottonblue <cottonblue713@gmail.com>
2021-01-09 13:33:42 +0900

    change file names

---
+++ chap3.hs
@@ -0,0 +1,77 @@
+lucky :: Int -> String
+lucky 7 = "LUCKY NUMBER SEVEN!"
+lucky x = "Sorry, you're out of luck, pal!"
+
+factorial :: Int -> Int
+factorial 0 = 1
+factorial n = n * factorial (n - 1)
+
+addVectors :: (Double, Double) -> (Double, Double) -> (Double, Double)
+-- not pattern matching 
+-- addVectors a b = (fst a + fst b, snd a + snd b)
+-- pattern matching
+addVectors (x1, y1) (x2, y2) = (x1 + x2, y1 + y2)
+
+first :: (a, b, c) -> a
+first (x, _, _) = x
+
+second :: (a, b, c) -> b
+second (_, y, _) = y
+
+third :: (a, b, c) -> c
+third (_, _, z) = z
+
+head' :: [a] -> a
+head' [] = error "Can't call head on an empty list, dummy!"
+head' (x:_) = x
+
+tell :: (Show a) => [a] -> String
+tell [] = "The list is empty"
+tell (x:[]) = "The list has one element: " ++ show x
+tell (x:y:[]) = "The list has two element: " ++ show x ++ " and " ++ show y
+tell (x:y:_) = "The list is long. The first two elements are: " ++ show x ++ " and " ++ show y
+
+firstLetter :: String -> String
+firstLetter "" = "Empty string, whoops!"
+firstLetter all@(x:xs) = "The first letter of " ++ all ++ " is " ++ [x]
+
+bmiTell :: Double -> Double -> String
+bmiTell weight height
+    | bmi <= skinny = "You're underweight, you emo, you!" 
+    | bmi <= normal = "You're supposedly normal. Pffft, I bet you're ugly!" 
+    | bmi <= fat = "You're fat! Lose some weight, fatty!"
+    | otherwise = "You're a whale, congratulations!"
+    where bmi = weight / height ^ 2
+          (skinny, normal, fat) = (18.5, 25.0, 30.0)
+
+max' :: (Ord a) => a -> a -> a
+max' a b
+    | a <= b = b
+    | otherwise = a
+
+myCompare :: (Ord a) => a -> a -> Ordering
+a `myCompare` b
+    | a == b = EQ
+    | a <= b = LT
+    | otherwise = GT
+
+initials :: String -> String -> String
+initials firstname lastname = [f] ++ ". " ++ [l] ++ "."
+    where (f:_) = firstname
+          (l:_) = lastname
+
+calcBmis :: [(Double, Double)] -> [Double]
+calcBmis xs = [bmi | (w, h) <- xs, let bmi = w / h ^ 2, bmi > 25.0]
+
+cylinder :: Double -> Double -> Double
+cylinder r h =
+    let sideArea = 2 * pi * r * h
+        topArea = pi * r ^ 2
+    in sideArea + 2 * topArea
+
+describeList :: [a] -> String
+describeList ls = "The list is " ++ what ls 
+    where what [] = "empty."
+          what [x] = "a singleton list."
+          what xs = "a longer list."
+
